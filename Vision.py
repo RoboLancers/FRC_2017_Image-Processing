7cb96392d45b350bfe932fa9a8f4dd2d3354e4b8
bchen290 <brianchen0810@hotmail.com>
2017-01-22 13:15:19 -0500

    Add trackbars and fix convention

--- Vision.py
+++ Vision.py
@@ -1,48 +1,59 @@
 import cv2
-import numpy as np
 
-class Vision():
 
-    def processImage(self, image, lower, upper):
-        #Convert the frame to the hsv colorspace
-        hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+class Vision:
+    def do_nothing(self):
+        pass
 
-        #Creates a mask for the color green
-        mask = cv2.inRange(hsvImage, lower, upper)
+    '#Pre process the image to get the mask'
 
-        #Erode and dilate the mask to remove blobs
+    @staticmethod
+    def process_image(image, lower, upper):
+        # Convert the frame to the hsv colorspace
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Creates a mask for the color green
+        mask = cv2.inRange(hsv_image, lower, upper)
+
+        # Erode and dilate the mask to remove blobs
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
 
         return mask
 
-    def findContours(self, maskImage):
-        #Find the contour in the mask
-        contours = cv2.findContours(maskImage.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    '#Finds the contours in the image'
 
-        #Create an array to hold all contour area
-        areaArray = []
+    @staticmethod
+    def find_contours(mask_image):
+        # Find the contour in the mask
+        contours = cv2.findContours(mask_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-        #Only if we find a contour do we continue
+        # Create an array to hold all contour area
+        area_array = []
+
+        # Only if we find a contour do we continue
         if len(contours) > 0:
-            #Find the largest contour
-            firstLargestContour = max(contours, key=cv2.contourArea)
+            # Find the largest contour
+            first_largest_contour = max(contours, key=cv2.contourArea)
 
-            #Fill the array with contour areas
+            # Fill the array with contour areas
             for i, j in enumerate(contours):
                 area = cv2.contourArea(j)
-                areaArray.append(area)
+                area_array.append(area)
 
-            #Sort the array based on contour area
-            sortedArray  = sorted(zip(areaArray,contours), key=lambda x: x[0], reverse=True)
+            # Sort the array based on contour area
+            sortedArray = sorted(zip(area_array, contours), key=lambda x: x[0], reverse=True)
 
-            #Find the nth largest contour [n-1][1] only if there is more than one contour
+            # Find the nth largest contour [n-1][1] only if there is more than one contour
             if len(contours) > 1:
-                secondLargestContour = sortedArray[1][1]
+                second_largest_contour = sortedArray[1][1]
 
-            return firstLargestContour, secondLargestContour
+            return first_largest_contour, second_largest_contour
 
-    def drawContours(self, image, largestContour, secondLargestContour):
+    '#Draws contour on the image'
+
+    @staticmethod
+    def draw_contours(image, largestContour, secondLargestContour):
         x, y, w, h = cv2.boundingRect(largestContour)
         cv2.drawContours(image, largestContour, -1, (255, 0, 0), 2)
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -51,36 +62,50 @@ class Vision():
         cv2.drawContours(image, secondLargestContour, -1, (255, 0, 0), 2)
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    #Define both the lower and upper boundary of the ball tracking
-    #TODO add trackbar
-    greenLower = (0,0,0)
-    greenUpper = (1,1,1)
+    '#Create a window for the trackbars'
+    cv2.namedWindow("Trackbars", cv2.WINDOW_NORMAL)
+
+    '#Create trackbars to filter image'
+    cv2.createTrackbar("Hue Lower", "Trackbars", 0, 180, do_nothing())
+    cv2.createTrackbar("Hue Upper", "Trackbars", 180, 180, do_nothing())
+
+    cv2.createTrackbar("Saturation Lower", "Trackbars", 0, 255, do_nothing())
+    cv2.createTrackbar("Saturation Upper", "Trackbars", 255, 255, do_nothing())
+
+    cv2.createTrackbar("Value Lower", "Trackbars", 0, 255, do_nothing())
+    cv2.createTrackbar("Value Upper", "Trackbars", 255, 255, do_nothing())
 
     camera = cv2.VideoCapture(0)
 
     while True:
-        #Read the frame from the camera
+        # Define both the lower and upper boundary of the ball tracking
+        greenLower = (cv2.getTrackbarPos("Hue Lower", "Trackbars"), cv2.getTrackbarPos("Saturation Lower", "Trackbars"),
+                      cv2.getTrackbarPos("Value Lower", "Trackbars"))
+        greenUpper = (cv2.getTrackbarPos("Hue Upper", "Trackbars"), cv2.getTrackbarPos("Saturation Upper", "Trackbars"),
+                      cv2.getTrackbarPos("Value Upper", "Trackbars"))
+
+        # Read the frame from the camera
         (grabbed, frame) = camera.read()
 
-        #Process the image to get a mask
-        maskImage = processImage(frame, greenLower, greenUpper)
+        # Process the image to get a mask
+        maskImage = process_image(frame, greenLower, greenUpper)
 
-        #Find the largest and second largest contour
-        firstContour, secondContour = findContours(maskImage)
+        # Find the largest and second largest contour
+        firstContour, secondContour = find_contours(maskImage)
 
-        #Draws the contours on the mask
-        drawContours(maskImage, firstContour, secondContour)
+        # Draws the contours on the mask
+        draw_contours(maskImage, firstContour, secondContour)
 
-        #Shows the image to the screen
+        # Shows the image to the screen
         cv2.imshow("Frame", frame)
         cv2.imshow("Mask Frame", maskImage)
 
         key = cv2.waitKey(1) & 0xFF
 
-        #If q key is pressed then we quit
+        # If q key is pressed then we quit
         if key == ord("q"):
             break
 
-    #Clean up the camera and close all windows
+    # Clean up the camera and close all windows
     camera.release()
     cv2.destroyAllWindows()
