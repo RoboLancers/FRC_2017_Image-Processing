06c8a8e4381d8c1bb6743e2c80c43edc67742435
Brian Chen <brianchen0810@gmail.com>
2017-01-27 18:58:06 -0500

    Fix network tables

--- Vision.py
+++ Vision.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 from networktables import NetworkTable
 import os
-import socket
 
 
-os.system("uvcdynctrl -s 'Exposure, Auto' 1")
-os.system("uvcdynctrl -s 'Exposure (Absolute)' 5")
+os.system("uvcdynctrl -s 'Exposure, Auto' 3")
+os.system("uvcdynctrl -s Brightness 62")
+os.system("uvcdynctrl -s Contrast 4")
+os.system("uvcdynctrl -s Saturation 79")
+os.system("uvcdynctrl -s 'White Balance Temperature, Auto' 0")
+os.system("uvcdynctrl -s 'White Balance Temperature' 4487")
+os.system("uvcdynctrl -s 'Sharpness' 25")
 
-NetworkTable.setIPAddress('10.51.15.2')
+NetworkTable.setIPAddress('10.3.21.2')
 NetworkTable.setClientMode()
 NetworkTable.initialize()
 
 nt = NetworkTable.getTable('jetson')
-
-lancer_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_address = ('10.3.21.2', 10000)
-lancer_socket.connect((server_address, 5800))
 
 def do_nothing(x):
     pass
@@ -46,40 +46,38 @@ def get_angle(frames, contour):
     angle_offset = 73 * pixel_offset / width
     angle_offset -= 3.5
 
-    print(str(angle_offset))
-
     return angle_offset
+
+def find_center(contour1, contour2):
+    center_x1, center_y1 = calculate_centroid(contour1)
+    center_x2, center_y2 = calculate_centroid(contour2)
+    return (center_x1 + center_x2) / 2
 
 
 '#Create a window for the trackbars'
-cv2.namedWindow("Trackbars", cv2.WINDOW_NORMAL)
+#cv2.namedWindow("Trackbars", cv2.WINDOW_NORMAL)
 
 '#Create trackbars to filter image'
-cv2.createTrackbar("Hue Lower", "Trackbars", 0, 180, do_nothing)
-cv2.createTrackbar("Hue Upper", "Trackbars", 180, 180, do_nothing)
+#cv2.createTrackbar("Hue Lower", "Trackbars", 0, 180, do_nothing)
+#cv2.createTrackbar("Hue Upper", "Trackbars", 180, 180, do_nothing)
 
-cv2.createTrackbar("Saturation Lower", "Trackbars", 0, 255, do_nothing)
-cv2.createTrackbar("Saturation Upper", "Trackbars", 255, 255, do_nothing)
+#cv2.createTrackbar("Saturation Lower", "Trackbars", 0, 255, do_nothing)
+#cv2.createTrackbar("Saturation Upper", "Trackbars", 255, 255, do_nothing)
 
-cv2.createTrackbar("Value Lower", "Trackbars", 0, 255, do_nothing)
-cv2.createTrackbar("Value Upper", "Trackbars", 255, 255, do_nothing)
+#cv2.createTrackbar("Value Lower", "Trackbars", 0, 255, do_nothing)
+#cv2.createTrackbar("Value Upper", "Trackbars", 255, 255, do_nothing)
 
 camera = cv2.VideoCapture(0)
-camera.set(3, 160)
-camera.set(4, 120)
+#camera.set(3, 160)
+#camera.set(4, 120)
 
 while True:
     # Define both the lower and upper boundary of the ball tracking
-
-    #Value without uvcdynctrl
-    #greenLower = np.array([49, 110, 104])
-    #greenUpper = np.array([180, 255, 255])
-
     #Value with uvcdynctrl
-    greenLower = np.array([cv2.getTrackbarPos("Hue Lower", "Trackbars"), cv2.getTrackbarPos("Saturation Lower", "Trackbars"), cv2.getTrackbarPos("Value Lower", "Trackbars")])
-    greenUpper = np.array([cv2.getTrackbarPos("Hue Upper", "Trackbars"), cv2.getTrackbarPos("Saturation Upper", "Trackbars"), cv2.getTrackbarPos("Value Upper", "Trackbars")])
-
-    kernel = np.ones((5, 5), np.uint8)
+    #greenLower = np.array([cv2.getTrackbarPos("Hue Lower", "Trackbars"), cv2.getTrackbarPos("Saturation Lower", "Trackbars"), cv2.getTrackbarPos("Value Lower", "Trackbars")])
+    #greenUpper = np.array([cv2.getTrackbarPos("Hue Upper", "Trackbars"), cv2.getTrackbarPos("Saturation Upper", "Trackbars"), cv2.getTrackbarPos("Value Upper", "Trackbars")])
+    greenLower = np.array([48,103,135])
+    greenUpper = np.array([137,225,255])
 
     # Read the frame from the camera
     (grabbed, frame) = camera.read()
@@ -91,9 +89,11 @@ while True:
     # Creates a mask for the color green
     mask_image = cv2.inRange(hsv_image, greenLower, greenUpper)
 
+    blurred_image = cv2.bilateralFilter(mask_image,9,75,75)
+
     # Erode and dilate the mask to remove blobs
-    mask = cv2.erode(mask_image, kernel, iterations=1)
-    mask_image = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.erode(mask_image, None, iterations=2)
+    mask_image = cv2.dilate(mask_image, None, iterations=2)
 
     # Find the contour in the mask
     contours = cv2.findContours(mask_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
@@ -116,18 +116,22 @@ while True:
 
         # Sort the array based on contour area
         sortedArray = sorted(zip(area_array, contours), key=lambda x: x[0], reverse=True)
+        b = 0
 
         if len(contours) > 1:
             # Find the nth largest contour [n-1][1] only if there is more than one contour
-            second_largest_contour = sortedArray[1]
-
-            if first_largest_contour is second_largest_contour:
-                print("Equal")
+            second_largest_contour = sortedArray[1][1]
+            cv2.drawContours(frame, second_largest_contour, -1, (255,0,0), 2)
+            b = get_angle(frame, second_largest_contour)
+            b = round(b, 2)
 
         a = get_angle(frame, first_largest_contour)
-        lancer_socket.sendall(str(a) + "\n")
+        a = round(a, 2)
+        average_middle = (a+b)/2
+        print(str(average_middle))
+        #lancer_socket.sendall(str(a) + "\n")
         if nt.isConnected() and a != 36 and a != 32.5:
-            nt.putNumber('angletotarget', a)
+            nt.putNumber('angletogoal', average_middle)
 
     # Shows the image to the screen
     cv2.imshow("Frame", frame)
